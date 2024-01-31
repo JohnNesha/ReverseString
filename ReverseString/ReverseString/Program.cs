4d0acf1edf042059c0c3aa48b19692ca81701d77
JohnNesha <150056202+JohnNesha@users.noreply.github.com>
2024-01-30 23:12:22 -0500

    Reverse String sln

--- ReverseString/ReverseString/Program.cs
+++ ReverseString/ReverseString/Program.cs
@@ -5,6 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Reverse String! Enter a word or phrase you would like to see in reverse\n");
+
+        string userWord = string.Empty;
+        userWord = Console.ReadLine().ToLower();
+
+        string reverseWord = string.Empty;
+        for (int i = userWord.Length - 1; i >= 0; i --)
+        {
+            reverseWord += userWord[i];
+        }
+        Console.WriteLine($"Your word in reverse is:\n {reverseWord}\n");
+
+        //checking for a palidrome
+        if (reverseWord == userWord)
+        {
+            Console.WriteLine("The word you entered is a palidrome!\n");
+        }
+        else
+        {
+            Console.WriteLine("Your word is not a palidrome");
+        }
     }
 }
 
